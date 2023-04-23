a0db58c45657cdefc4288cb2b6beca3a389240cd
svedev0 <emanuel@kulich.me>
2023-04-23 23:35:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from tkinter import Tk, filedialog
 from tika import parser
 
-file_picker = Tk()
-file_picker.withdraw()
-file = filedialog.askopenfilename()
+def run():
+	file_picker = Tk()
+	file_picker.withdraw()
+	file = filedialog.askopenfilename()
 
-with open(file, 'rb') as f:
-	input = parser.from_file(f)
-	content = input['content']
-	content = content.strip()
+	with open(file, 'rb') as f:
+		input = parser.from_file(f)
+		content = input['content']
+		content = content.strip()
 
-output = open('pdf_content.txt', mode='w+', encoding='utf-8')
-output.write(content)
-output.close()
+	output = open('pdf_content.txt', mode='w+', encoding='utf-8')
+	output.write(content)
+	output.close()
+
+if __name__ == '__main__':
+	run()
